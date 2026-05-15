dce7eb365fe2ce87c819c0087725f04a4aa57ee6
juniorrecordscontact-cmd <junior.records.contact@gmail.com>
2026-05-15 01:48:14 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const beats = [
+  {
+    title: "Summer Guitar Beat",
+    price: 25,
+    previewFile: "https://www.dropbox.com/scl/fi/3n3sfuytmc7flqn5wxal6/115-BPM-Summer-Guitar.mp3?rlkey=syfcxzuokrclnb7ksnarm1xey&raw=1",
+    wavFile: "https://www.dropbox.com/scl/fi/a0njsx2e82ybvs3dik7zz/115-BPM-Summer-Guitar.wav?rlkey=lw6ftovagn0wcjj9skl7e4uxr&raw=1",
+    image: "https://www.dropbox.com/scl/fi/ksun4elxr76vvoqlmltst/guitar.jpg?rlkey=djhn73qvr0g5vt6gknw22uwcb&raw=1",
+    buyLink: "https://www.paypal.com/paypalme/jayanreid07/25"
+  }
+];
